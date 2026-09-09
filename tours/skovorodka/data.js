3844b1546969f000cb357567f508e8815fc8e991
ProTour3D <lenaaaaa2009@gmail.com>
2026-09-09 10:57:51 +0400

    загрузка сковородка кр яр

---
+++ tours/skovorodka/data.js
@@ -0,0 +1,257 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "Творческое пространство",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.3571879937214177,
+        "pitch": 0.1561990605111543,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7584037936192658,
+          "pitch": 0.31712540212587825,
+          "rotation": 0,
+          "target": "1--"
+        },
+        {
+          "yaw": -1.9648254282672717,
+          "pitch": 0.08204225021532352,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": 2.7704112769682316,
+          "pitch": 0.20688773121666415,
+          "rotation": 0,
+          "target": "4---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--",
+      "name": "Основная зона",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -2.7168361294792263,
+        "pitch": 0.2858339980437421,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.39101005844563,
+          "pitch": 0.5492298107428439,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": -1.9464041149479776,
+          "pitch": 0.22799983383843525,
+          "rotation": 12.566370614359176,
+          "target": "3--"
+        },
+        {
+          "yaw": 1.515834558071215,
+          "pitch": 0.29076137736332264,
+          "rotation": 0,
+          "target": "0--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "Крафт-зона",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 2.7918245187079025,
+        "pitch": 0.2117036804250816,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7946139522562161,
+          "pitch": 0.18719455065373225,
+          "rotation": 0,
+          "target": "0--"
+        },
+        {
+          "yaw": 1.3132460569139823,
+          "pitch": 0.468284031608146,
+          "rotation": 0,
+          "target": "1--"
+        },
+        {
+          "yaw": -1.1024605123062727,
+          "pitch": 0.4083072493237161,
+          "rotation": 0,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "Каминный зал",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -2.1338543215713486,
+        "pitch": 0.122549764972824,
+        "fov": 1.3484478478922592
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7173235747990185,
+          "pitch": 0.34834030368803326,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4---",
+      "name": "Открытая терраса ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.4408679597113565,
+        "pitch": 0.07999933585314345,
+        "fov": 1.56374737030644
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2265822159064292,
+          "pitch": 0.04636674259237594,
+          "rotation": 0,
+          "target": "0--"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "skovorodka kr yr",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
