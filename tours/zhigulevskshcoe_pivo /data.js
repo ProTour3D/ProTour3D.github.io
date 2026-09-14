23050980cacf166f3880f396fa60725a2272452b
ProTour3D <lenaaaaa2009@gmail.com>
2026-09-14 19:00:55 +0400

    Загрузка Пивнушка

---
+++ tours/zhigulevskshcoe_pivo /data.js
@@ -0,0 +1,343 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0----",
+      "name": "ул. Ново-Вокзальный тупик",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.5038516799358383,
+        "pitch": -0.19662040365410505,
+        "fov": 0.6641507063327897
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6504831323650713,
+          "pitch": 0.11399871766133707,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "Крыльцо",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.287697275943252,
+        "pitch": 0.14279966607225703,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3819545719045578,
+          "pitch": 0.26714355139151813,
+          "rotation": 0,
+          "target": "0----"
+        },
+        {
+          "yaw": -1.4448984651019856,
+          "pitch": 0.0732364315579499,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "Входная зона",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.3579757728533277,
+        "pitch": 0.21837662799340052,
+        "fov": 1.5766316568845151
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5167627386667304,
+          "pitch": 0.43514932553702224,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -1.7132218017797882,
+          "pitch": 0.5084628882194551,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": -0.18237751653499856,
+          "pitch": 0.4622077638528772,
+          "rotation": 0,
+          "target": "5--"
+        },
+        {
+          "yaw": -0.9813293688933271,
+          "pitch": 0.37498664508232693,
+          "rotation": 0,
+          "target": "4----"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.21543093874277,
+          "pitch": 0.5437336308721719,
+          "title": "<ya-tr-span data-index=\"195-0\" data-translated=\"true\" data-source-lang=\"en\" data-target-lang=\"ru\" data-value=\"Title\" data-translation=\"Заголовок\" data-ch=\"0\" data-type=\"trSpan\" style=\"visibility: inherit !important;\" data-selected=\"false\">Холодильник с мороженым</ya-tr-span>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3--",
+      "name": "Витрина закусок",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.0721384806886824,
+        "pitch": 0.14240239727630843,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7896838580101715,
+          "pitch": 0.5642534887948507,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": 0.20321451461649076,
+          "pitch": 0.5381157760612396,
+          "rotation": 0,
+          "target": "4----"
+        },
+        {
+          "yaw": 0.9696711737601191,
+          "pitch": 0.3431808132614016,
+          "rotation": 0,
+          "target": "5--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4----",
+      "name": "Зона с рыбной закуской",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.3372161476958944,
+        "pitch": 0.3627434841270727,
+        "fov": 1.5766316568845151
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5255335186774124,
+          "pitch": 0.5924383003406675,
+          "rotation": 0,
+          "target": "5--"
+        },
+        {
+          "yaw": -2.9223083299767403,
+          "pitch": 0.5146249659274797,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": 2.514625778214814,
+          "pitch": 0.39104626349373817,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5071876885854696,
+          "pitch": 0.2879466641799624,
+          "title": "<ya-tr-span data-index=\"229-0\" data-translated=\"true\" data-source-lang=\"en\" data-target-lang=\"ru\" data-value=\"Title\" data-translation=\"Заголовок\" data-ch=\"0\" data-type=\"trSpan\" style=\"visibility: inherit !important;\">Копченая рыба</ya-tr-span>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.34940230855340104,
+          "pitch": 0.6887929566604356,
+          "title": "<ya-tr-span data-index=\"233-0\" data-translated=\"true\" data-source-lang=\"en\" data-target-lang=\"ru\" data-value=\"Title\" data-translation=\"Название\" data-ch=\"0\" data-type=\"trSpan\" style=\"visibility: inherit !important;\">Холодильник с креветками</ya-tr-span>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5--",
+      "name": "Зона снеков",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 2.0550873367090254,
+        "pitch": 0.2087985744241596,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6057280001005658,
+          "pitch": 0.5930395249648868,
+          "rotation": 0,
+          "target": "4----"
+        },
+        {
+          "yaw": -2.965392064009473,
+          "pitch": 0.5942740190044411,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": -1.8831712792408108,
+          "pitch": 0.5103454589940863,
+          "rotation": 0,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Zhigulevskshcoe_pivo",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
