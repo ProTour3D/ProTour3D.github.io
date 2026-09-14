dd9574a839101e237f1c535864c422cf2bbfe7db
ProTour3D <lenaaaaa2009@gmail.com>
2026-09-14 17:36:21 +0400

    Загрузка КонстанАвто

---
+++ tours/KonstanAuto/data.js
@@ -0,0 +1,185 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cam_20260914125133_0068_d",
+      "name": "CAM_20260914125133_0068_D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.9971541051692299,
+        "pitch": 0.0970957370734915,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8294431706802374,
+          "pitch": 0.6401316906583183,
+          "rotation": 0,
+          "target": "1-cam_20260914125323_0069_d"
+        },
+        {
+          "yaw": 1.377766233296823,
+          "pitch": 0.04973845520570208,
+          "rotation": 0,
+          "target": "2-cam_20260914125355_0070_d"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cam_20260914125323_0069_d",
+      "name": "CAM_20260914125323_0069_D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 2.253380899169624,
+        "pitch": -0.06108672480230837,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2851132583212426,
+          "pitch": 0.5259518665390246,
+          "rotation": 0,
+          "target": "0-cam_20260914125133_0068_d"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-cam_20260914125355_0070_d",
+      "name": "CAM_20260914125355_0070_D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.6573234261746546,
+        "pitch": -0.016630645128174137,
+        "fov": 1.0415538085727225
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8296904248325312,
+          "pitch": 0.13871612279122303,
+          "rotation": 0,
+          "target": "0-cam_20260914125133_0068_d"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-cam_20260914125437_0071_d",
+      "name": "CAM_20260914125437_0071_D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.382912236175068,
+          "pitch": -0.18213386110143404,
+          "rotation": 0,
+          "target": "2-cam_20260914125355_0070_d"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "KonstantAuto",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
