28a9a46a0608afe8cd981fec9d373d261112f14f
ProTour3D <lenaaaaa2009@gmail.com>
2026-09-16 15:30:36 +0400

    Загрузка Атаман

---
+++ tours/Ataman/data.js
@@ -0,0 +1,251 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "Кафе Атаман",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.7760627397786664,
+        "pitch": -0.21567193186859157,
+        "fov": 1.0929080473751445
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9073687159260952,
+          "pitch": 0.1388557119092102,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -1.7153699579413786,
+          "pitch": 0.02945921959286224,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--",
+      "name": "Входная группа",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -0.4008842255503975,
+        "pitch": 0.10328260596398842,
+        "fov": 1.5766316568845151
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.514784245779543,
+          "pitch": 0.16436056412387856,
+          "rotation": 0,
+          "target": "0--"
+        },
+        {
+          "yaw": 3.000448550199385,
+          "pitch": 0.42131209593238594,
+          "rotation": 0,
+          "target": "2---"
+        },
+        {
+          "yaw": -0.2540946929645358,
+          "pitch": 0.5264962013997216,
+          "rotation": 0,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2---",
+      "name": "Место у мангала",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 2.6417763430922028,
+        "pitch": 0.05937783359032878,
+        "fov": 1.3720022333166877
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.011502992611738705,
+          "pitch": 0.4819923462866402,
+          "rotation": 0,
+          "target": "1--"
+        },
+        {
+          "yaw": -0.13613845725076956,
+          "pitch": 0.2598054471298674,
+          "rotation": 0,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "Гостевой зал",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -2.4115608155587083,
+        "pitch": 0.15477970646871597,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6378061941643587,
+          "pitch": 0.44842309129400704,
+          "rotation": 0,
+          "target": "1--"
+        },
+        {
+          "yaw": 2.73823235029153,
+          "pitch": 0.21168474624139932,
+          "rotation": 0,
+          "target": "2---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "Шаурма",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 0.36309201831386595,
+        "pitch": -0.23024654188433225,
+        "fov": 1.0693545797769706
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7272236211943675,
+          "pitch": 0.14363176009420897,
+          "rotation": 0,
+          "target": "0--"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Ataman",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
