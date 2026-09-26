aa9b172b8226ecad482f07bc31fcf6b61b52917c
ProTour3D <lenaaaaa2009@gmail.com>
2026-09-26 09:48:41 +0400

    загрузка прогулка

---
+++ tours/progulka/data.js
@@ -0,0 +1,191 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cam_20260925163220_0078_d",
+      "name": "CAM_20260925163220_0078_D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.2214047951502636,
+        "pitch": 0.12044377212052026,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.651862432959927,
+          "pitch": 0.41597098648778363,
+          "rotation": 0,
+          "target": "1-cam_20260925163257_0079_d"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cam_20260925163257_0079_d",
+      "name": "CAM_20260925163257_0079_D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.0676583366378498,
+        "pitch": 0.11839587807709684,
+        "fov": 1.5766316568845151
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8461165481213673,
+          "pitch": 0.08183750547128099,
+          "rotation": 0,
+          "target": "0-cam_20260925163220_0078_d"
+        },
+        {
+          "yaw": -1.1904007230033677,
+          "pitch": 0.4456427374292957,
+          "rotation": 0,
+          "target": "2-cam_20260925163401_0081_d"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-cam_20260925163401_0081_d",
+      "name": "CAM_20260925163401_0081_D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7379283570662203,
+          "pitch": 0.03367711717864097,
+          "rotation": 0,
+          "target": "1-cam_20260925163257_0079_d"
+        },
+        {
+          "yaw": 0.23730241696683052,
+          "pitch": 0.40928309195980894,
+          "rotation": 0,
+          "target": "3-cam_20260925163451_0082_d"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-cam_20260925163451_0082_d",
+      "name": "CAM_20260925163451_0082_D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -0.1543060749485754,
+        "pitch": 0.0679405331072136,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9542498211706327,
+          "pitch": 0.029619009332611412,
+          "rotation": 0,
+          "target": "2-cam_20260925163401_0081_d"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "progulka",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
